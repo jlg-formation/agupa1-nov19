1ba2fb243b69857f61101fb4008ddc6f933d5978
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-05 13:52:12 +0100

    editor config

--- back/server.js
+++ back/server.js
@@ -7,5 +7,5 @@ app.use(express.static('.'));
 app.use(serveIndex('.', { 'icons': true }));
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!');
 });
