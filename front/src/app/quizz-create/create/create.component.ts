5f73ded566959a1bc0868dee8c9636cdfa88de4e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-06 16:58:06 +0100

    submit

--- front/src/app/quizz-create/create/create.component.ts
+++ front/src/app/quizz-create/create/create.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
+  f = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  submit() {
+    console.log('titi');
   }
-
 }
