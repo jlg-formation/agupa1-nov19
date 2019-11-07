47a7d1697c260c2036bcb9394a12ad53a10bc6c1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-07 15:04:32 +0100

    score ok

--- front/src/app/quizz-play/score/score.component.ts
+++ front/src/app/quizz-play/score/score.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faSmileWink } from '@fortawesome/free-regular-svg-icons';
+import { QuizzService } from 'src/app/service/quizz.service';
 
 @Component({
   selector: 'app-score',
@@ -10,7 +11,7 @@ export class ScoreComponent implements OnInit {
 
   faSmileWink = faSmileWink;
 
-  constructor() { }
+  constructor(public quizz: QuizzService) { }
 
   ngOnInit() {
   }
