ceb8ec16bb37c35d79e6fd40e92edd257d009bc9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-08 14:48:37 +0100

    button radio

--- front/src/app/widget/widget.module.ts
+++ front/src/app/widget/widget.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { AutofocusDirective } from './autofocus.directive';
 import { TimerComponent } from './timer/timer.component';
 import { FormatDurationPipe } from './format-duration.pipe';
+import { RadioButtonComponent } from './radio-button/radio-button.component';
 
 
 
 @NgModule({
-  declarations: [AutofocusDirective, TimerComponent, FormatDurationPipe],
+  declarations: [AutofocusDirective, TimerComponent, FormatDurationPipe, RadioButtonComponent],
   imports: [
     CommonModule
   ],
-  exports: [AutofocusDirective, TimerComponent, FormatDurationPipe]
+  exports: [AutofocusDirective, TimerComponent, FormatDurationPipe, RadioButtonComponent]
 })
 export class WidgetModule { }
