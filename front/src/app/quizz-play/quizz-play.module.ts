e83ee9e30ca0920f8ece05fb2830fe2cb7c8c2fa
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-08 11:02:33 +0100

    added timer

--- front/src/app/quizz-play/quizz-play.module.ts
+++ front/src/app/quizz-play/quizz-play.module.ts
@@ -7,6 +7,7 @@ import { ListComponent } from './list/list.component';
 import { QuestionComponent } from './question/question.component';
 import { ScoreComponent } from './score/score.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { WidgetModule } from '../widget/widget.module';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     FontAwesomeModule,
     QuizzPlayRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    WidgetModule,
   ]
 })
 export class QuizzPlayModule { }
--- front/src/app/widget/widget.module.ts
+++ front/src/app/widget/widget.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AutofocusDirective } from './autofocus.directive';
+import { TimerComponent } from './timer/timer.component';
 
 
 
 @NgModule({
-  declarations: [AutofocusDirective],
+  declarations: [AutofocusDirective, TimerComponent],
   imports: [
     CommonModule
   ],
-  exports: [AutofocusDirective]
+  exports: [AutofocusDirective, TimerComponent]
 })
 export class WidgetModule { }
